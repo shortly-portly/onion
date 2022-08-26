4f12d48f95a2c30794e4e15f1fb6675081423408
Dave Simmons <shortlyportly@googlemail.com>
2022-08-26 20:33:50 +0100

    Added Tailwind

--- config/dev.exs
+++ config/dev.exs
@@ -26,6 +26,7 @@ config :onion, OnionWeb.Endpoint,
   secret_key_base: "nrs7UNhx+QMmmL62EbRkcUXwVG1Py2w7r3rUxXt5K08OTpPV+eNKkVtOlcoP1wAw",
   watchers: [
     # Start the esbuild watcher by calling Esbuild.install_and_run(:default, args)
+    tailwind: {Tailwind, :install_and_run, [:default, ~w(--watch)]},
     esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]}
   ]
 
